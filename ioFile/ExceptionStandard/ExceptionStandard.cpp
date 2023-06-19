aad711afce4161b41e5f67e1f7fcd7a738bc0e8f
Mashell0707 <merlsutama@gmail.com>
2023-06-19 15:45:35 +0700

    exceptionstandard done

--- ioFile/ExceptionStandard/ExceptionStandard.cpp
+++ ioFile/ExceptionStandard/ExceptionStandard.cpp
@@ -9,11 +9,13 @@ int main()
 {
 	cout << "Awal program" << endl;
 	try {
-		array<int, 3> data = { 1. 2. 3 };
+		array<int, 3> data = { 1, 2, 3 };
 		cout << data.at(4) << endl;
 
 	}
 	catch (exception& e) {
 		cout << e.what() << endl;
 	}
+	cout << "baris program yang terakhir" << endl;
+	return 0;
 }
